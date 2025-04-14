38d525a5d4d43ed90ad3c6a2404291708775cb8b
RA <permale.ra.tech@gmail.com>
2025-04-14 19:45:53 +1000

    library setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
 
+console.log(PORT);
+
+app.use(express.json());
+app.use(cors());
 app.use(helmet()); //middleware security, helps protect app by adding various http headers
 app.use(morgan("dev")); //request logger
 
@@ -12,6 +21,6 @@ app.get("/", (req, res) => {
     res.send("Hello from backend");
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
 });
