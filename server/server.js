515260b32837591bf3834988ea6a7e2d748be3cb
RA <permale.ra.tech@gmail.com>
2025-04-16 02:51:53 +1000

    Database initialized

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import productRoutes from "./routes/productRoutes.js"
+import pool from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -20,6 +21,44 @@ app.use(morgan("dev")); //request logger
 
 app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT);
-});
+async function initDB() {
+    try {
+        const createUsersTable = await pool.query(
+            `
+            CREATE TABLE IF NOT EXISTS Users (
+                user_id    SERIAL PRIMARY KEY,
+                username   VARCHAR(50) NOT NULL,
+                email      VARCHAR(100) NOT NULL UNIQUE,
+                password   VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+            `
+        )
+
+        const createReviewsTable = await pool.query(
+            `
+            CREATE TABLE IF NOT EXISTS Reviews (
+            review_id     INT           GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            user_id       INT           NOT NULL, 
+            product_id    INT           NOT NULL,
+            rating        INT           NOT NULL CHECK (rating BETWEEN 1 AND 5),
+            comment       TEXT,
+            created_at    TIMESTAMP     DEFAULT CURRENT_TIMESTAMP,
+            CONSTRAINT fk_user
+            FOREIGN KEY (user_id)
+            REFERENCES Users (user_id)
+            );
+            `
+        )
+
+        console.log("Database initialized")
+    } catch (error) {
+        console.log("Error initDB", error);
+    }
+}
+
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is running on port " + PORT);
+    });
+}) 
